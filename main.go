efd7fc6808221538b27916936b0cca792dd65e2e
zerobugdebug <github@zerobugdebug.ca>
2023-10-04 09:20:12 -0400

    Add central logging

--- main.go
+++ main.go
@@ -1,22 +1,12 @@
 package main
 
 import (
-	"github.com/sirupsen/logrus"
-
 	"github.com/zerobugdebug/cogfight/pkg/fighter"
 	"github.com/zerobugdebug/cogfight/pkg/game"
+	"github.com/zerobugdebug/cogfight/pkg/log"
 )
 
-var log *logrus.Logger
-
 func main() {
-	log := logrus.New()
-	// Set log format to include timestamp and colors
-	log.SetFormatter(&logrus.TextFormatter{
-		TimestampFormat: "2006-01-02 15:04:05.000",
-		ForceColors:     true,
-		FullTimestamp:   true,
-	})
 
 	// Welcome message
 	log.Info("Welcome to the CogFight!")
